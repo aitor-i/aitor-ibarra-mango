467d27e635059fa57c92ec872d195ba3f301c7af
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-13 12:09:35 +0200

    ADD - get fixed range api endpoint

---
+++ src/app/api/get-fixed-range/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+
+  const rangeValues = [1.99, 5.99, 10.99, 30.99, 50.99, 70.99]
+  const body = { rangeValues }
+
+  return new NextResponse(JSON.stringify(body), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
