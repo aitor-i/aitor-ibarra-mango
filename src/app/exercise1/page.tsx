cd9465a19e86c186619f3e3121d553f7a211b5c3
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-13 12:11:02 +0200

    ADD - Exercise 1 page ssr
    fetch data from api & render range

---
+++ src/app/exercise1/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Range } from '@/components/Range/Range'
+interface Ranges {
+  min: number,
+  max: number
+}
+
+export default async function page() {
+
+  const endpoint = new URL(process.env.NEXT_PUBLIC_BASE_URL + "/api/get-range")
+  const res = await fetch(endpoint)
+  if (!res.ok) throw new Error('Error')
+  const data: Ranges = await res.json()
+
+
+  return (
+    <div className="justify-center gap-4 flex-col flex px-4 py-32 h-screen alingn-center">
+      <Range max={data.max} min={data.min} />
+    </div>
+  )
+}
