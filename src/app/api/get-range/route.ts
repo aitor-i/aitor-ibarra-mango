e545b6738815800972b04013eb4e74a5f5c825e4
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-13 12:09:51 +0200

    ADD - get range api endpoint

---
+++ src/app/api/get-range/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+
+  const body = { max: 100, min: 1 }
+
+  return new NextResponse(JSON.stringify(body), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
