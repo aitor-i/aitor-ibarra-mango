a7690314956ec851d17c0e4d34c5e009c1c1db74
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-14 18:43:05 +0200

    ADD - Api error handling

--- src/app/exercise1/page.tsx
+++ src/app/exercise1/page.tsx
@@ -9,12 +9,12 @@ interface Ranges {
 export default async function page() {
     const endpoint = new URL(process.env.NEXT_PUBLIC_BASE_URL + '/api/get-range');
     const res = await fetch(endpoint);
-    if (!res.ok) throw new Error('Error');
-    const data: Ranges = await res.json();
+
+    const data: Ranges = res.ok ? await res.json() : {};
 
     return (
         <div className="justify-center gap-4 flex-col flex px-4 py-32 h-screen alingn-center">
-            <Range max={data.max} min={data.min} />
+            {res.ok && <Range max={data.max} min={data.min} />}
         </div>
     );
 }
--- src/app/exercise2/page.tsx
+++ src/app/exercise2/page.tsx
@@ -8,12 +8,12 @@ interface Ranges {
 export default async function page() {
     const endpoint = new URL(process.env.NEXT_PUBLIC_BASE_URL + '/api/get-fixed-range');
     const res = await fetch(endpoint);
-    if (!res.ok) throw new Error('Error');
-    const data: Ranges = await res.json();
+
+    const data: Ranges = res.ok ? await res.json() : {};
 
     return (
         <div className="justify-center gap-4 flex-col flex px-4 py-32 h-screen alingn-center">
-            <Range fixedValues={data.rangeValues} />
+            {res.ok && <Range fixedValues={data.rangeValues} />}
         </div>
     );
 }
