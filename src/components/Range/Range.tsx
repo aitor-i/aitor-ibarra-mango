9bcd6586e702f30739ffee1561cd43c247311188
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-14 17:43:53 +0200

    FORMAT

--- src/components/Range/Range.tsx
+++ src/components/Range/Range.tsx
@@ -2,176 +2,172 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 interface RangeSliderProps {
-  min?: number;
-  max?: number;
-  fixedValues?: number[];
+    min?: number;
+    max?: number;
+    fixedValues?: number[];
 }
 
-function getRangeBoundary(rangeValues: RangeSliderProps): { max: number, min: number } {
+function getRangeBoundary(rangeValues: RangeSliderProps): { max: number; min: number } {
+    if (rangeValues.fixedValues && (rangeValues.min || rangeValues.max)) throw "Invalid props, you can't set fixed values and max or min values!";
 
-  if (rangeValues.fixedValues && (rangeValues.min || rangeValues.max)) throw "Invalid props, you can't set fixed values and max or min values!";
+    if (rangeValues.fixedValues) {
+        const max = Math.max(...rangeValues.fixedValues);
+        const min = Math.min(...rangeValues.fixedValues);
 
-  if (rangeValues.fixedValues) {
-    const max = Math.max(...rangeValues.fixedValues);
-    const min = Math.min(...rangeValues.fixedValues);
-
-    return { max, min }
-
-  }
-  if (rangeValues.min !== undefined && rangeValues.max !== undefined) {
-    return { max: rangeValues.max, min: rangeValues.min }
-  }
-  else {
-    throw 'No max and min or fixed values found!'
-  }
+        return { max, min };
+    }
+    if (rangeValues.min !== undefined && rangeValues.max !== undefined) {
+        return { max: rangeValues.max, min: rangeValues.min };
+    } else {
+        throw 'No max and min or fixed values found!';
+    }
 }
-
 
 export const Range = ({ min, max, fixedValues }: RangeSliderProps) => {
-  const [range, setRange] = useState<{ start: number; end: number }>({ start: 0, end: 0 });
-  const [isDragging, setIsDragging] = useState<{ start: boolean; end: boolean }>({ start: false, end: false });
-  const rangeRef = useRef<HTMLDivElement>(null);
-  const startThumbRef = useRef<HTMLDivElement>(null);
-  const endThumbRef = useRef<HTMLDivElement>(null);
+    const [range, setRange] = useState<{ start: number; end: number }>({ start: 0, end: 0 });
+    const [isDragging, setIsDragging] = useState<{ start: boolean; end: boolean }>({ start: false, end: false });
+    const rangeRef = useRef<HTMLDivElement>(null);
+    const startThumbRef = useRef<HTMLDivElement>(null);
+    const endThumbRef = useRef<HTMLDivElement>(null);
 
-  const rangeBoundary = getRangeBoundary({ min, max, fixedValues })
+    const rangeBoundary = getRangeBoundary({ min, max, fixedValues });
 
-  useEffect(() => {
-    setRange({ start: rangeBoundary.min ?? 0, end: rangeBoundary.max ?? 0 })
-  }, []);
+    useEffect(() => {
+        setRange({ start: rangeBoundary.min ?? 0, end: rangeBoundary.max ?? 0 });
+    }, []);
 
-  useEffect(() => {
-    const moveThumb = (event: MouseEvent) => {
-      if (!rangeRef.current) return;
-      if (isDragging.start && startThumbRef.current) startThumbRef.current.style.cursor = 'grabbing';
-      if (isDragging.end && endThumbRef.current) endThumbRef.current.style.cursor = 'grabbing';
+    useEffect(() => {
+        const moveThumb = (event: MouseEvent) => {
+            if (!rangeRef.current) return;
+            if (isDragging.start && startThumbRef.current) startThumbRef.current.style.cursor = 'grabbing';
+            if (isDragging.end && endThumbRef.current) endThumbRef.current.style.cursor = 'grabbing';
 
-      const { clientX } = event;
-      const { left, width } = rangeRef.current.getBoundingClientRect();
-      const clickedPosition = Math.min(Math.max(0, clientX - left), width);
-      if (fixedValues) {
-        const sortedValues = fixedValues.sort((a, b) => a - b);
-        const clickedValue = clickedPosition / width;
-        const newValue = sortedValues.reduce((prev, curr) =>
-          Math.abs(curr - (sortedValues[0] + clickedValue * (sortedValues[sortedValues.length - 1] - sortedValues[0]))) <
-            Math.abs(prev - (sortedValues[0] + clickedValue * (sortedValues[sortedValues.length - 1] - sortedValues[0])))
-            ? curr
-            : prev
-        );
+            const { clientX } = event;
+            const { left, width } = rangeRef.current.getBoundingClientRect();
+            const clickedPosition = Math.min(Math.max(0, clientX - left), width);
+            if (fixedValues) {
+                const sortedValues = fixedValues.sort((a, b) => a - b);
+                const clickedValue = clickedPosition / width;
+                const newValue = sortedValues.reduce((prev, curr) =>
+                    Math.abs(curr - (sortedValues[0] + clickedValue * (sortedValues[sortedValues.length - 1] - sortedValues[0]))) <
+                    Math.abs(prev - (sortedValues[0] + clickedValue * (sortedValues[sortedValues.length - 1] - sortedValues[0])))
+                        ? curr
+                        : prev
+                );
 
-        if (isDragging.start) {
-          if (newValue >= range.end) {
-            setIsDragging({ ...isDragging, start: false });
-            return;
-          }
-          setRange(prev => ({ ...prev, start: newValue }));
-        } else if (isDragging.end) {
-          if (newValue <= range.start) {
-            setIsDragging({ ...isDragging, end: false });
-            return;
-          }
-          setRange(prev => ({ ...prev, end: newValue }));
+                if (isDragging.start) {
+                    if (newValue >= range.end) {
+                        setIsDragging({ ...isDragging, start: false });
+                        return;
+                    }
+                    setRange(prev => ({ ...prev, start: newValue }));
+                } else if (isDragging.end) {
+                    if (newValue <= range.start) {
+                        setIsDragging({ ...isDragging, end: false });
+                        return;
+                    }
+                    setRange(prev => ({ ...prev, end: newValue }));
+                }
+            } else {
+                const clickedValue = clickedPosition / width;
+                const newValue = +(rangeBoundary.min + (rangeBoundary.max - rangeBoundary.min) * clickedValue).toFixed(2);
+
+                if (isDragging.start) {
+                    if (newValue > range.end) {
+                        setIsDragging({ ...isDragging, start: false });
+                        return;
+                    }
+                    setRange({ ...range, start: newValue });
+                } else if (isDragging.end) {
+                    if (newValue < range.start) {
+                        setIsDragging({ ...isDragging, end: false });
+                        return;
+                    }
+                    setRange({ ...range, end: newValue });
+                }
+            }
+        };
+
+        if (isDragging.start || isDragging.end) {
+            window.addEventListener('mousemove', moveThumb);
+            window.addEventListener('mouseup', () => {
+                if (isDragging.start && startThumbRef.current) startThumbRef.current.style.cursor = 'grab';
+                if (isDragging.end && endThumbRef.current) endThumbRef.current.style.cursor = 'grab';
+                setIsDragging({ start: false, end: false });
+            });
         }
-      } else {
-        const clickedValue = clickedPosition / width;
-        const newValue = +(rangeBoundary.min + (rangeBoundary.max - rangeBoundary.min) * clickedValue).toFixed(2);
 
-        if (isDragging.start) {
-          if (newValue > range.end) {
-            setIsDragging({ ...isDragging, start: false });
-            return;
-          }
-          setRange({ ...range, start: newValue });
-        } else if (isDragging.end) {
-          if (newValue < range.start) {
-            setIsDragging({ ...isDragging, end: false });
-            return;
-          }
-          setRange({ ...range, end: newValue });
-        }
-      }
+        return () => {
+            window.removeEventListener('mousemove', moveThumb);
+            window.addEventListener('mouseup', () => {
+                if (!isDragging.end && !isDragging.start) return;
+                if (isDragging.start && startThumbRef.current) startThumbRef.current.style.cursor = 'grab';
+                if (isDragging.end && endThumbRef.current) endThumbRef.current.style.cursor = 'grab';
+                setIsDragging({ start: false, end: false });
+            });
+        };
+    }, [isDragging.start, isDragging.end, min, max, range, fixedValues]);
+
+    const startDragging = (thumb: 'start' | 'end') => () => {
+        setIsDragging({ ...isDragging, [thumb]: true });
     };
 
-    if (isDragging.start || isDragging.end) {
-      window.addEventListener('mousemove', moveThumb);
-      window.addEventListener('mouseup', () => {
-        if (isDragging.start && startThumbRef.current) startThumbRef.current.style.cursor = 'grab';
-        if (isDragging.end && endThumbRef.current) endThumbRef.current.style.cursor = 'grab';
-        setIsDragging({ start: false, end: false });
-      });
-    }
-
-    return () => {
-      window.removeEventListener('mousemove', moveThumb);
-      window.addEventListener('mouseup', () => {
-        if (!isDragging.end && !isDragging.start) return;
-        if (isDragging.start && startThumbRef.current) startThumbRef.current.style.cursor = 'grab';
-        if (isDragging.end && endThumbRef.current) endThumbRef.current.style.cursor = 'grab';
-        setIsDragging({ start: false, end: false });
-      });
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, type: 'start' | 'end') => {
+        const value = parseFloat(event.target.value.replace('€', '').trim());
+        if (!isNaN(value) && value >= rangeBoundary.min && value <= rangeBoundary.max) {
+            if (type === 'start' && value <= range.end) {
+                setRange({ ...range, start: value });
+            } else if (type === 'end' && value >= range.start) {
+                setRange({ ...range, end: value });
+            }
+        }
     };
-  }, [isDragging.start, isDragging.end, min, max, range, fixedValues]);
 
-  const startDragging = (thumb: 'start' | 'end') => () => {
-    setIsDragging({ ...isDragging, [thumb]: true });
-  };
+    return (
+        <div className="flex gap-4 justify-center items-center ">
+            <input
+                type="text"
+                disabled={fixedValues ? true : false}
+                className="text-right w-20 bg-transparent appearance-none border-none focus:outline-none focus:border-none focus:ring-0 text-sm"
+                value={range.start.toFixed(2) + '€'}
+                onChange={e => handleInputChange(e, 'start')}
+                min={min}
+                max={max}
+                step="0.01"
+                data-testid="start-input"
+            />
+            <div ref={rangeRef} className="h-1 w-full bg-black rounded-full relative cursor-pointer " data-testid="range-track">
+                <div
+                    className="absolute top-1/2 h-3 w-3 cursor-grab rounded-full -translate-x-1/2 -translate-y-1/2 bg-black hover:scale-110"
+                    style={{
+                        left: `${((range.start - rangeBoundary.min) / (rangeBoundary.max - rangeBoundary.min)) * 100}%`
+                    }}
+                    onMouseDown={startDragging('start')}
+                    data-testid="start-thumb"
+                    ref={startThumbRef}
+                />
+                <div
+                    className="absolute top-1/2 h-3 w-3 cursor-grab rounded-full -translate-x-1/2 -translate-y-1/2 bg-black hover:scale-110"
+                    style={{
+                        left: `${((range.end - rangeBoundary.min) / (rangeBoundary.max - rangeBoundary.min)) * 100}%`
+                    }}
+                    onMouseDown={startDragging('end')}
+                    ref={endThumbRef}
+                    data-testid="end-thumb"
+                />
+            </div>
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, type: 'start' | 'end') => {
-    const value = parseFloat(event.target.value.replace('€', '').trim());
-    if (!isNaN(value) && value >= rangeBoundary.min && value <= rangeBoundary.max) {
-      if (type === 'start' && value <= range.end) {
-        setRange({ ...range, start: value });
-      } else if (type === 'end' && value >= range.start) {
-        setRange({ ...range, end: value });
-      }
-    }
-  };
-
-  return (
-    <div className="flex gap-4 justify-center items-center ">
-      <input
-        type="text"
-        disabled={fixedValues ? true : false}
-        className="text-right w-20 bg-transparent appearance-none border-none focus:outline-none focus:border-none focus:ring-0 text-sm"
-        value={range.start.toFixed(2) + '€'}
-        onChange={e => handleInputChange(e, 'start')}
-        min={min}
-        max={max}
-        step="0.01"
-        data-testid="start-input"
-      />
-      <div ref={rangeRef} className="h-1 w-full bg-black rounded-full relative cursor-pointer " data-testid="range-track">
-        <div
-          className="absolute top-1/2 h-3 w-3 cursor-grab rounded-full -translate-x-1/2 -translate-y-1/2 bg-black hover:scale-110"
-          style={{
-            left: `${((range.start - rangeBoundary.min) / (rangeBoundary.max - rangeBoundary.min)) * 100}%`
-          }}
-          onMouseDown={startDragging('start')}
-          data-testid="start-thumb"
-          ref={startThumbRef}
-        />
-        <div
-          className="absolute top-1/2 h-3 w-3 cursor-grab rounded-full -translate-x-1/2 -translate-y-1/2 bg-black hover:scale-110"
-          style={{
-            left: `${((range.end - rangeBoundary.min) / (rangeBoundary.max - rangeBoundary.min)) * 100}%`
-          }}
-          onMouseDown={startDragging('end')}
-          ref={endThumbRef}
-          data-testid="end-thumb"
-        />
-      </div>
-
-      <input
-        type="text"
-        disabled={fixedValues ? true : false}
-        className="w-20 bg-transparent appearance-none border-none focus:outline-none focus:border-none focus:ring-0 text-sm"
-        value={range.end.toFixed(2) + '€'}
-        onChange={e => handleInputChange(e, 'end')}
-        min={min}
-        max={max}
-        step="0.01"
-        data-testid="end-input"
-      />
-    </div>
-  );
+            <input
+                type="text"
+                disabled={fixedValues ? true : false}
+                className="w-20 bg-transparent appearance-none border-none focus:outline-none focus:border-none focus:ring-0 text-sm"
+                value={range.end.toFixed(2) + '€'}
+                onChange={e => handleInputChange(e, 'end')}
+                min={min}
+                max={max}
+                step="0.01"
+                data-testid="end-input"
+            />
+        </div>
+    );
 };
